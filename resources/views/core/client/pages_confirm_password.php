217d5110a9dbcedf3d78d0546463cf053e6610d6
MartMbithi <martdevelopers254@gmail.com>
2021-03-21 11:12:00 +0300

    Refactored Password Reset Logic -Client Module

---
+++ resources/views/core/client/pages_confirm_password.php
@@ -0,0 +1,111 @@
+<?php
+session_start();
+include('conf/config.php');
+if (isset($_POST['confirm_reset_password'])) {
+    /* Confirm Password */
+    $error = 0;
+    if (isset($_POST['new_password']) && !empty($_POST['new_password'])) {
+        $new_password = mysqli_real_escape_string($mysqli, trim(sha1(md5($_POST['new_password']))));
+    } else {
+        $error = 1;
+        $err = "New Password Cannot Be Empty";
+    }
+    if (isset($_POST['confirm_password']) && !empty($_POST['confirm_password'])) {
+        $confirm_password = mysqli_real_escape_string($mysqli, trim(sha1(md5($_POST['confirm_password']))));
+    } else {
+        $error = 1;
+        $err = "Confirmation Password Cannot Be Empty";
+    }
+
+    if (!$error) {
+        $email = $_SESSION['email'];
+        $sql = "SELECT * FROM  iB_clients  WHERE email = '$email'";
+        $res = mysqli_query($mysqli, $sql);
+        if (mysqli_num_rows($res) > 0) {
+            $row = mysqli_fetch_assoc($res);
+            if ($new_password != $confirm_password) {
+                $err = "Password Does Not Match";
+            } else {
+                $email = $_SESSION['email'];
+                $query = "UPDATE iB_clients SET  password =? WHERE email =?";
+                $stmt = $mysqli->prepare($query);
+                $rc = $stmt->bind_param('ss', $new_password, $email);
+                $stmt->execute();
+                if ($stmt) {
+                    $success = "Password Changed" && header("refresh:1; url=pages_client_index.php");
+                } else {
+                    $err = "Please Try Again Or Try Later";
+                }
+            }
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+<?php include("dist/_partials/head.php"); ?>
+
+<body class="hold-transition login-page">
+    <div class="login-box">
+        <div class="login-logo">
+            <a href=""><b>i</b>Banking</a>
+        </div>
+        <!-- /.login-logo -->
+        <div class="card">
+            <div class="card-body login-card-body">
+                <?php
+                $email  = $_SESSION['email'];
+                $ret = "SELECT * FROM  iB_clients  WHERE email = '$email'";
+                $stmt = $mysqli->prepare($ret);
+                $stmt->execute(); //ok
+                $res = $stmt->get_result();
+                while ($row = $res->fetch_object()) {
+                ?>
+                    <p class="login-box-msg"><?php echo $row->name; ?> Please Enter And Confirm Your Password</p>
+                <?php } ?>
+                <form method="POST">
+                    <div class="input-group mb-3">
+                        <input type="password" required name="new_password" class="form-control" placeholder="New Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" required name="confirm_password" class="form-control" placeholder="Confirm Password">
+                        <div class="input-group-append">
+                            <div class="input-group-text">
+                                <span class="fas fa-lock"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <button type="submit" name="confirm_reset_password" class="btn btn-primary btn-block">Reset Password</button>
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                </form>
+
+                <p class="mt-3 mb-1">
+                    <!--           <a href="pages_staff_index.php">Login</a>
+ -->
+                </p>
+
+            </div>
+            <!-- /.login-card-body -->
+        </div>
+    </div>
+    <!-- /.login-box -->
+
+    <!-- jQuery -->
+    <script src="lugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
+
+</body>
+
+</html>
