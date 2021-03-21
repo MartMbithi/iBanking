278b684311dbe41c53dde6577a1d21bc7dbce4e8
MartMbithi <martdevelopers254@gmail.com>
2021-03-21 11:12:00 +0300

    Persist System Settings On Staff Auth

--- core/staff/pages_confirm_password.php
+++ core/staff/pages_confirm_password.php
@@ -40,72 +40,82 @@ if (isset($_POST['confirm_reset_password'])) {
         }
     }
 }
+
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `iB_SystemSettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($auth = $res->fetch_object()) {
 ?>
-<!DOCTYPE html>
-<html>
-<?php include("dist/_partials/head.php"); ?>
+    <!DOCTYPE html>
+    <html>
+    <?php include("dist/_partials/head.php"); ?>
 
-<body class="hold-transition login-page">
-    <div class="login-box">
-        <div class="login-logo">
-            <a href=""><b>i</b>Banking</a>
-        </div>
-        <!-- /.login-logo -->
-        <div class="card">
-            <div class="card-body login-card-body">
-                <?php
-                $email  = $_SESSION['email'];
-                $ret = "SELECT * FROM  iB_staff  WHERE email = '$email'";
-                $stmt = $mysqli->prepare($ret);
-                $stmt->execute(); //ok
-                $res = $stmt->get_result();
-                while ($row = $res->fetch_object()) {
-                ?>
-                    <p class="login-box-msg"><?php echo $row->name; ?> Please Enter And Confirm Your Password</p>
-                <?php } ?>
-                <form method="POST">
-                    <div class="input-group mb-3">
-                        <input type="password" required name="new_password" class="form-control" placeholder="New Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="input-group mb-3">
-                        <input type="password" required name="confirm_password" class="form-control" placeholder="Confirm Password">
-                        <div class="input-group-append">
-                            <div class="input-group-text">
-                                <span class="fas fa-lock"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <button type="submit" name="confirm_reset_password" class="btn btn-primary btn-block">Reset Password</button>
-                        </div>
-                        <!-- /.col -->
-                    </div>
-                </form>
-
-                <p class="mt-3 mb-1">
-                    <!--           <a href="pages_staff_index.php">Login</a>
- -->
-                </p>
-
+    <body class="hold-transition login-page">
+        <div class="login-box">
+            <div class="login-logo">
+                <p><?php echo $auth->sys_name; ?> - <?php echo $auth->sys_tagline; ?></p>
             </div>
-            <!-- /.login-card-body -->
+            <!-- /.login-logo -->
+            <div class="card">
+                <div class="card-body login-card-body">
+                    <?php
+                    $email  = $_SESSION['email'];
+                    $ret = "SELECT * FROM  iB_staff  WHERE email = '$email'";
+                    $stmt = $mysqli->prepare($ret);
+                    $stmt->execute(); //ok
+                    $res = $stmt->get_result();
+                    while ($row = $res->fetch_object()) {
+                    ?>
+                        <p class="login-box-msg"><?php echo $row->name; ?> Please Enter And Confirm Your Password</p>
+                    <?php
+                    } ?>
+                    <form method="POST">
+                        <div class="input-group mb-3">
+                            <input type="password" required name="new_password" class="form-control" placeholder="New Password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="input-group mb-3">
+                            <input type="password" required name="confirm_password" class="form-control" placeholder="Confirm Password">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-lock"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <button type="submit" name="confirm_reset_password" class="btn btn-primary btn-block">Reset Password</button>
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                    </form>
+
+                    <p class="mt-3 mb-1">
+                        <!--           <a href="pages_staff_index.php">Login</a>
+ -->
+                    </p>
+
+                </div>
+                <!-- /.login-card-body -->
+            </div>
         </div>
-    </div>
-    <!-- /.login-box -->
+        <!-- /.login-box -->
 
-    <!-- jQuery -->
-    <script src="lugins/jquery/jquery.min.js"></script>
-    <!-- Bootstrap 4 -->
-    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-    <!-- AdminLTE App -->
-    <script src="dist/js/adminlte.min.js"></script>
+        <!-- jQuery -->
+        <script src="lugins/jquery/jquery.min.js"></script>
+        <!-- Bootstrap 4 -->
+        <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- AdminLTE App -->
+        <script src="dist/js/adminlte.min.js"></script>
 
-</body>
+    </body>
 
-</html>
+    </html>
+<?php
+} ?>
--- core/staff/pages_reset_pwd.php
+++ core/staff/pages_reset_pwd.php
@@ -37,55 +37,64 @@ if (isset($_POST['reset_password'])) {
     $err = "Email Does Not Exist";
   }
 }
+
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `iB_SystemSettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($auth = $res->fetch_object()) {
 ?>
-<!DOCTYPE html>
-<html>
-<?php include("dist/_partials/head.php"); ?>
+  <!DOCTYPE html>
+  <html>
+  <?php include("dist/_partials/head.php"); ?>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href=""><b>i</b>Banking</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <p><?php echo $auth->sys_name; ?> - <?php echo $auth->sys_tagline; ?></p>
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
 
-        <form method="POST">
-          <div class="input-group mb-3">
-            <input type="email" required name="email" class="form-control" placeholder="Email">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
+          <form method="POST">
+            <div class="input-group mb-3">
+              <input type="email" required name="email" class="form-control" placeholder="Email">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-12">
-              <button type="submit" name="reset_password" class="btn btn-primary btn-block">Request new password</button>
+            <div class="row">
+              <div class="col-12">
+                <button type="submit" name="reset_password" class="btn btn-primary btn-block">Request new password</button>
+              </div>
+              <!-- /.col -->
             </div>
-            <!-- /.col -->
-          </div>
-        </form>
+          </form>
 
-        <p class="mt-3 mb-1">
-          <a href="pages_staff_index.php">Login</a>
-        </p>
+          <p class="mt-3 mb-1">
+            <a href="pages_staff_index.php">Login</a>
+          </p>
 
+        </div>
+        <!-- /.login-card-body -->
       </div>
-      <!-- /.login-card-body -->
     </div>
-  </div>
-  <!-- /.login-box -->
+    <!-- /.login-box -->
 
-  <!-- jQuery -->
-  <script src="lugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
+    <!-- jQuery -->
+    <script src="lugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
 
-</body>
+  </body>
 
-</html>
+  </html>
+<?php
+} ?>
