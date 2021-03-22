774af2052d9e0d4cc817fbc55af0a9a3d987dce1
MartMbithi <martdevelopers254@gmail.com>
2021-03-22 10:54:14 +0300

    Refactored Landing Pages

--- core/index.php
+++ core/index.php
@@ -1,357 +1,72 @@
-<!doctype html>
-<html class="no-js" lang="">
+<?php
+include("admin/conf/config.php");
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `iB_SystemSettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($sys = $res->fetch_object()) {
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <meta http-equiv="content-type" content="text/html;charset=utf-8" />
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>iBanking - Safe, Fast, Secure And Effective Banking</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="assets/images/favicon.png">
-    <!-- Place favicon.ico in the root directory -->
-    <!-- all css here -->
-    <!-- bootstrap v3.3.7 css -->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!-- animate css -->
-    <link rel="stylesheet" href="assets/css/animate.css">
-    <!-- owl.carousel.2.0.0-beta.2.4 css -->
-    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
-    <!-- swiper.min.css -->
-    <link rel="stylesheet" href="assets/css/swiper.min.css">
-    <!-- font-awesome v4.6.3 css -->
-    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
-    <!-- flaticon.css -->
-    <link rel="stylesheet" href="assets/css/flaticon.css">
-    <!-- magnific-popup.css -->
-    <link rel="stylesheet" href="assets/css/magnific-popup.css">
-    <!-- metisMenu.min.css -->
-    <link rel="stylesheet" href="assets/css/metisMenu.min.css">
-    <!-- style css -->
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <!-- responsive css -->
-    <link rel="stylesheet" href="assets/css/responsive.css">
-    <!-- modernizr css -->
-    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <title><?php echo $sys->sys_name; ?> - <?php echo $sys->sys_tagline; ?></title>
+        <link href="dist/css/robust.css" rel="stylesheet">
+    </head>
 
-<body>
+    <body>
 
-
-    <!-- header-area start -->
-    <header class="header-area header-area3 header-area2">
-        <div class="header-top">
+        <nav class="navbar navbar-lg navbar-expand-lg navbar-transparant navbar-dark navbar-absolute w-100">
             <div class="container">
-                <div class="row">
-                    <div class="col-xl-5 col-lg-6 col-md-8 col-12">
-                        <ul class="contact-info d-flex">
-                            <li>
-                                <span><i class="fa fa-phone"></i> +254 737 229776</span>
-                            </li>
-                            <li>
-                                <span><i class="fa fa-envelope"></i> info@ibanking.com</span>
-                                <span><i class="fa fa-paper-plane-o"></i> www.martdev.info/ibanking</span>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-xl-7 col-lg-6 col-sm-4 col-12 d-none d-md-block">
-                        <ul class="social-icon d-flex justify-content-end">
-                            <li class="facebook"><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li class="twitter"><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li class="google-plus"><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                            <li class="linkedin"><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li class="youtube"><a href="#"><i class="fa fa-youtube"></i></a></li>
-                        </ul>
-                    </div>
+                <a class="navbar-brand" href="index.php"><?php echo $sys->sys_name; ?></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" target="_blank" href="admin/pages_index.php">Admin Portal</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" target="_blank" href="staff/pages_staff_index.php">Staff Portal</a>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" target="_blank" href="client/pages_client_index.php">Client Portal</a>
+                        </li>
+                    </ul>
+                    <a class="btn btn-outline-white" href="client/pages_client_signup.php" target="_blank">Join Us</a>
                 </div>
             </div>
-        </div>
-        <div class="header-bottom">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-sm-7 col-12">
-                        <div class="logo">
-                            <a href="index.php">
-                                <img src="assets/images/logo3.png" alt="">
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-7 col-sm-9 d-none d-lg-block">
-                        <ul class="mainmenu d-flex justify-content-end">
-                            <li class="active"><a href="index.php">Home </a>
-                            <li><a target="_blank" href="client/pages_client_index.php">Client Portal </a></li>
-                            <li><a target="_blank" href="staff/pages_staff_index.php">Staff Portal </a></li>
-                            <li><a target="_blank" href="admin/pages_index.php">Administrator Portal</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-2 col-sm-3 col-8">
-                        <div class="geta_quote">
-                            <a target="_blank" href="client/pages_client_signup.php">Join Us</a>
-                        </div>
-                    </div>
-                    <div class="d-block d-lg-none col-4 pull-right col-sm-2">
-                        <ul class="menu">
-                            <li class="first"></li>
-                            <li class="second"></li>
-                            <li class="third"></li>
-                        </ul>
-                    </div>
-                </div>
+        </nav>
+
+        <div class="intro py-5 py-lg-9 position-relative text-white">
+            <div class="bg-overlay-primary">
+                <img src="img/photos/7.jpg" class="img-fluid img-cover" alt="Robust UI Kit" />
             </div>
-            <!-- responsive-menu area start -->
-            <div class="responsive-menu-area d-block d-sm-none">
+            <div class="intro-content py-6 text-center">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-12">
-                            <ul class="metismenu">
-                                <li class="active"><a href="index.php">Home </a>
-                                <li><a target="_blank" href="client/pages_client_index.php">Client Portal </a></li>
-                                <li><a target="_blank" href="staff/pages_staff_index.php">Staff Portal </a></li>
-                                <li><a target="_blank" href="admin/pages_index.php">Administrator Portal</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- responsive-menu area start -->
-        </div>
-
-    </header>
-    <!-- header-area end -->
-
-    <!-- slider-area start -->
-    <div class="slider-area slider-area3">
-        <div class="slider-active2 owl-carousel">
-            <div class="slider-items">
-                <img src="assets/images/slider/1.jpg" alt="" class="slider">
-                <div class="slider-content flex-style">
-                    <div class="container">
-                        <div class="row">
-                            <div class="text-center col-12 col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                                <h2><span>iBank</span></h2>
-                                <p>Your Trusted eBanking Agent</p>
-                                <p>Safe, Fast, Secure And Effective Banking</p>
-                                <ul>
-                                    <li><a href="client/pages_client_index.php">JOIN NOW</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slider-items">
-                <img src="assets/images/slider/2.jpg" alt="" class="slider">
-                <div class="slider-content flex-style">
-                    <div class="container">
-                        <div class="row">
-                            <div class="text-center col-12 col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                                <h2><span>iBank</span></h2>
-                                <p>Your Trusted eBanking Agent</p>
-                                <p>Safe, Fast, Secure And Effective Banking</p>
-                                <ul>
-                                    <li><a href="client/pages_client_index.php">JOIN NOW</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slider-items">
-                <img src="assets/images/slider/3.jpg" alt="" class="slider">
-                <div class="slider-content flex-style">
-                    <div class="container">
-                        <div class="row">
-                            <div class="text-center col-12 col-xl-8 offset-xl-2 col-lg-10 offset-lg-1">
-                                <h2><span>iBank</span></h2>
-                                <p>Your Trusted eBanking Agent</p>
-                                <p>Safe, Fast, Secure And Effective Banking</p>
-                                <ul>
-                                    <li><a href="client/pages_client_index.php">JOIN NOW</a></li>
-                                </ul>
-                            </div>
+                    <div class="row align-items-center">
+                        <div class="col-12 col-sm-10 col-md-8 col-lg-6 mx-auto text-center">
+                            <h1 class="my-3 display-4 d-none d-lg-inline-block"><?php echo $sys->sys_name; ?></h1>
+                            <p class="lead mb-3">
+                                <?php echo $sys->sys_tagline; ?>
+                            </p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="slider-thumbni-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="slider-thumbnil-active  owl-carousel">
-                            <div class="item">
-                                <img src="assets/images/slider/thumb/1.jpg" alt="">
-                            </div>
-                            <div class="item">
-                                <img src="assets/images/slider/thumb/2.jpg" alt="">
-                            </div>
+        <script src="dist/js/bundle.js"></script>
+    </body>
 
-                            <div class="item">
-                                <img src="assets/images/slider/thumb/3.jpg" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- slider-area end -->
-
-    <!-- featured-area start -->
-    <div class="featured-area featured-area3">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 col-sm-6 col-12">
-                    <div class="featured-wrap">
-                        <span class="flaticon-bitcoin-9"></span>
-                        <h2><a href="#">Secure</a></h2>
-                        <p>Our transactions and your money is safe and secure with us.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 col-12">
-                    <div class="featured-wrap active">
-                        <span class="flaticon-bitcoin-8"></span>
-                        <h2><a href="#">Effective</a></h2>
-                        <p>Ibank is effective and efficient which guarantees 100% uptime.</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6 col-12">
-                    <div class="featured-wrap">
-                        <span class="flaticon-exchange-1"></span>
-                        <h2><a href="#">Time Saver</a></h2>
-                        <p>iBank money transactions are automated and encrypted with a complex mechanism which improves the performance.</p>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </div>
-    <!-- featured-area end -->
-
-    <!-- about-area start -->
-    <div class="about-area position-relative">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-title text-center">
-                        <h2>About iBank</h2>
-                        <img src="assets/images/line.png" alt="">
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-6 d-none d-lg-block">
-                    <div class="about-img">
-                        <img src="assets/images/about/1.jpg" alt="">
-                    </div>
-                </div>
-                <div class="col-lg-6 col-12">
-                    <div class="about-content">
-                        <h3>iBank is an Online Banking Agency which its main goal is to increase security, effectiveness and efficiency in the banking sector by providing a secure, seamless and an effective platform for clients to
-                            keep their funds securely.</h3>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-4 col-6">
-                            <div class="about-items">
-                                <img src="assets/images/about/icon/1.png" alt="">
-                                <span class="counter">10</span>
-                                <p>Support Currencies</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 col-6">
-                            <div class="about-items">
-                                <img src="assets/images/about/icon/2.png" alt="">
-                                <span class="counter">7</span>
-                                <p>Bank Intergrations</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-4 col-6">
-                            <div class="about-items mb-0">
-                                <img src="assets/images/about/icon/3.png" alt="">
-                                <span class="counter">1250</span>
-                                <p>iBank Users</p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- about-area end -->
-
-    <!-- footer-area start -->
-    <footer class="footer-area">
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 copyright">
-                        <p>
-                            <!-- Link back to MartDevelopers can't be removed. -->
-                            iBank Copyright &copy;<script>
-                                document.write(new Date().getFullYear());
-                            </script> All rights reserved | Proudly Powered by <a href="https://martdev.info" target="_blank" class="text-primary">MartDevelopers</a>
-                            <!-- Link back to MartDevelopers can't be removed. -->
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <!-- footer-area end -->
-
-    <!-- jquery latest version -->
-    <script src="assets/js/vendor/jquery-2.2.4.min.js "></script>
-    <!-- popper.min.js -->
-    <script src="assets/js/vendor/popper.min.js "></script>
-    <!-- bootstrap js -->
-    <script src="assets/js/bootstrap.min.js "></script>
-    <!-- owl.carousel.2.0.0-beta.2.4 css -->
-    <script src="assets/js/owl.carousel.min.js "></script>
-    <!-- swiper.min.js -->
-    <script src="assets/js/swiper.min.js "></script>
-    <!-- mailchimp.js -->
-    <script src="assets/js/mailchimp.js"></script>
-    <!-- plugins js -->
-    <script src="assets/js/jquery.canvasjs.min.js"></script>
-    <!-- metisMenu.min.js -->
-    <script src="assets/js/metisMenu.min.js"></script>
-    <!-- plugins js -->
-    <script src="assets/js/plugins.js "></script>
-    <script>
-        // chart
-        window.onload = function() {
-            var limit = 10000; //increase number of dataPoints by increasing the limit
-            var y = 100;
-            var data = [];
-            var dataSeries = {
-                type: "line"
-            };
-            var dataPoints = [];
-            for (var i = 0; i < limit; i += 1) {
-                y += Math.round(Math.random() * 10 - 5);
-                dataPoints.push({
-                    x: i,
-                    y: y
-                });
-            }
-            dataSeries.dataPoints = dataPoints;
-            data.push(dataSeries);
-
-            //Better to construct options first and then pass it as a parameter
-            var options = {
-                zoomEnabled: true,
-                animationEnabled: true,
-                axisY: {
-                    includeZero: false
-                },
-                data: data // random data
-            };
-
-            $("#chartContainer").CanvasJSChart(options);
-        }
-    </script>
-    <!-- main js -->
-    <script src="assets/js/scripts.js "></script>
-</body>
-
-</html>
+    </html>
+<?php
+} ?>
