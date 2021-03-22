6caab82cf979bbf09d07aec97cfca4a668ae30eb
MartMbithi <martdevelopers254@gmail.com>
2021-03-22 10:39:00 +0300

    Persisted System Settings On Client Auth

--- core/client/pages_client_signup.php
+++ core/client/pages_client_signup.php
@@ -31,112 +31,119 @@ if (isset($_POST['create_account'])) {
   }
 }
 
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `iB_SystemSettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($auth = $res->fetch_object()) {
 ?>
-<!DOCTYPE html>
-<html>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<?php include("dist/_partials/head.php"); ?>
+  <!DOCTYPE html>
+  <html>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <?php include("dist/_partials/head.php"); ?>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href=""><b>i</b>Banking</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign In To Start Client Session</p>
-
-        <form method="post">
-          <div class="input-group mb-3">
-            <input type="text" name="name" required class="form-control" placeholder="Client Full Name">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-user"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="text" required name="national_id" class="form-control" placeholder="National ID Number">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-tag"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3" style="display:none">
-            <?php
-            //PHP function to generate random 
-            $length = 4;
-            $_Number =  substr(str_shuffle('0123456789'), 1, $length);
-            ?>
-            <input type="text" name="client_number" value="iBank-CLIENT-<?php echo $_Number; ?>" class="form-control" placeholder="Client Number">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="text" name="phone" required class="form-control" placeholder="Client Phone Number">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-phone"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="text" name="address" required class="form-control" placeholder="Client Address">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-map-marker"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="email" name="email" required class="form-control" placeholder="Client Address">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" name="password" required class="form-control" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-8">
-            </div>
-            <!-- /.col -->
-            <div class="col-4">
-              <button type="submit" name="create_account" class="btn btn-primary btn-block">Sign Up</button>
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
-
-        <p class="mb-0">
-          <a href="pages_client_index.php" class="text-center">Login</a>
-        </p>
-
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <p><?php echo $auth->sys_name; ?> - <?php echo $auth->sys_tagline; ?></p>
       </div>
-      <!-- /.login-card-body -->
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign In To Start Client Session</p>
+
+          <form method="post">
+            <div class="input-group mb-3">
+              <input type="text" name="name" required class="form-control" placeholder="Client Full Name">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-user"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" required name="national_id" class="form-control" placeholder="National ID Number">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-tag"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3" style="display:none">
+              <?php
+              //PHP function to generate random
+              $length = 4;
+              $_Number =  substr(str_shuffle('0123456789'), 1, $length); ?>
+              <input type="text" name="client_number" value="iBank-CLIENT-<?php echo $_Number; ?>" class="form-control" placeholder="Client Number">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" name="phone" required class="form-control" placeholder="Client Phone Number">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-phone"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="text" name="address" required class="form-control" placeholder="Client Address">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-map-marker"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="email" name="email" required class="form-control" placeholder="Client Address">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
+              </div>
+            </div>
+            <div class="input-group mb-3">
+              <input type="password" name="password" required class="form-control" placeholder="Password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-8">
+              </div>
+              <!-- /.col -->
+              <div class="col-4">
+                <button type="submit" name="create_account" class="btn btn-primary btn-block">Sign Up</button>
+              </div>
+              <!-- /.col -->
+            </div>
+          </form>
+
+          <p class="mb-0">
+            <a href="pages_client_index.php" class="text-center">Login</a>
+          </p>
+
+        </div>
+        <!-- /.login-card-body -->
+      </div>
     </div>
-  </div>
-  <!-- /.login-box -->
+    <!-- /.login-box -->
 
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
 
-</body>
+  </body>
 
-</html>
+  </html>
+<?php
+} ?>
