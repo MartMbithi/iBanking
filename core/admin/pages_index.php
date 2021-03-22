bb7ab060bcc1e931005233bca9baa6b166389811
MartMbithi <martdevelopers254@gmail.com>
2021-03-22 10:34:56 +0300

    Persisted System Settings On Admin Auth

--- core/admin/pages_index.php
+++ core/admin/pages_index.php
@@ -19,82 +19,91 @@ if (isset($_POST['login'])) {
     $err = "Access Denied Please Check Your Credentials";
   }
 }
+
+/* Persisit System Settings On Brand */
+$ret = "SELECT * FROM `iB_SystemSettings` ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($auth = $res->fetch_object()) {
 ?>
-<!DOCTYPE html>
-<html>
-<meta http-equiv="content-type" content="text/html;charset=utf-8" />
-<?php include("dist/_partials/head.php"); ?>
+  <!DOCTYPE html>
+  <html>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+  <?php include("dist/_partials/head.php"); ?>
 
-<body class="hold-transition login-page">
-  <div class="login-box">
-    <div class="login-logo">
-      <a href=""><b>i</b>Banking</a>
-    </div>
-    <!-- /.login-logo -->
-    <div class="card">
-      <div class="card-body login-card-body">
-        <p class="login-box-msg">Sign In To Start Adminstrator Session</p>
+  <body class="hold-transition login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <p><?php echo $auth->sys_name; ?> - <?php echo $auth->sys_tagline; ?></p>
+      </div>
+      <!-- /.login-logo -->
+      <div class="card">
+        <div class="card-body login-card-body">
+          <p class="login-box-msg">Sign In To Start Adminstrator Session</p>
 
-        <form method="post">
-          <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control" placeholder="Email">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
+          <form method="post">
+            <div class="input-group mb-3">
+              <input type="email" name="email" class="form-control" placeholder="Email">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-envelope"></span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
+            <div class="input-group mb-3">
+              <input type="password" name="password" class="form-control" placeholder="Password">
+              <div class="input-group-append">
+                <div class="input-group-text">
+                  <span class="fas fa-lock"></span>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-8">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember">
-                <label for="remember">
-                  Remember Me
-                </label>
+            <div class="row">
+              <div class="col-8">
+                <div class="icheck-primary">
+                  <input type="checkbox" id="remember">
+                  <label for="remember">
+                    Remember Me
+                  </label>
+                </div>
               </div>
+              <!-- /.col -->
+              <div class="col-4">
+                <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
+              </div>
+              <!-- /.col -->
             </div>
-            <!-- /.col -->
-            <div class="col-4">
-              <button type="submit" name="login" class="btn btn-primary btn-block">Sign In</button>
-            </div>
-            <!-- /.col -->
-          </div>
-        </form>
+          </form>
 
 
-        <!-- /.social-auth-links -->
+          <!-- /.social-auth-links -->
 
-        <p class="mb-1">
-          <a href="pages_reset_pwd.php">I forgot my password</a>
-        </p>
-        <!--
+          <p class="mb-1">
+            <a href="pages_reset_pwd.php">I forgot my password</a>
+          </p>
+          <!--
           Uncomment this line to allow account creations for admins
           
       <p class="mb-0">
         <a href="pages_signup.php" class="text-center">Register a new membership</a>
       </p>
       -->
+        </div>
+        <!-- /.login-card-body -->
       </div>
-      <!-- /.login-card-body -->
     </div>
-  </div>
-  <!-- /.login-box -->
+    <!-- /.login-box -->
 
-  <!-- jQuery -->
-  <script src="plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="dist/js/adminlte.min.js"></script>
+    <!-- jQuery -->
+    <script src="plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/adminlte.min.js"></script>
 
-</body>
+  </body>
 
-</html>
+  </html>
+<?php
+} ?>
